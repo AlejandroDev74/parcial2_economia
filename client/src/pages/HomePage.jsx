93b4f6bfc36624ced8e5154125cfbf2bf0b32459
AlejandroDev74 <calderonalejandrodev@gmail.com>
2024-10-07 18:30:13 -0500

    Adición de la url como variable del front 4

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-  <section className="bg-red-500 flex justify-center items-center">
+  <section className="bg-zinc-500 flex justify-center items-center">
     <header className="bg-zinc-800 p-10">
       <h1 className="text-5xl py-2 font-bold">Eventos React</h1>
       <p className="text-md text-slate-400">
