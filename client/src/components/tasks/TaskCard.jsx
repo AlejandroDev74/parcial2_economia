7d50c562c41dd882b031f3b5ffba8cc9475803bd
AlejandroDev74 <calderonalejandrodev@gmail.com>
2024-10-07 19:46:14 -0500

    Adición de la url como variable del front 6

--- client/src/components/tasks/TaskCard.jsx
+++ client/src/components/tasks/TaskCard.jsx
@@ -10,7 +10,6 @@ export function TaskCard({ task }) {
         <h1 className="text-2xl font-bold">{task.title}</h1>
         <div className="flex gap-x-2 items-center">
           <Button onClick={() => deleteTask(task._id)}>Eliminar</Button>
-          <ButtonLink to={`/tasks/${task._id}`}>Editar</ButtonLink>
         </div>
       </header>
       <p className="text-slate-300">{task.description}</p>
