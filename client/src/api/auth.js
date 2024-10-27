27a503c020d4aec0509924b0c3af55268f54a952
AlejandroDev74 <calderonalejandrodev@gmail.com>
2024-10-26 22:39:14 -0500

    Ajustes adicionales AlejandroDev74 26/10/2024

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,8 +1,8 @@
 import axios from "./axios";
 
 export const registerRequest = async (user) =>
-  axios.post(`https://parcial2back-theta.vercel.app/auth/register`, user);
+  axios.post(`/auth/register`, user);
 
-export const loginRequest = async (user) => axios.post(`https://parcial2back-theta.vercel.app/auth/login`, user);
+export const loginRequest = async (user) => axios.post(`/auth/login`, user);
 
-export const verifyTokenRequest = async () => axios.get(`https://parcial2back-theta.vercel.app/auth/verify`);
+export const verifyTokenRequest = async () => axios.get(`/auth/verify`);
