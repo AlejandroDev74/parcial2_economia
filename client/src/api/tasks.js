774c4da884f4bce167c4149660de9e6512385b9a
AlejandroDev74 <calderonalejandrodev@gmail.com>
2024-10-26 22:33:10 -0500

    Ajustes adicionales AlejandroDev74 26/10/2024

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -1,12 +1,12 @@
 import axios from "./axios";
 
-export const getTasksRequest = async () => axios.get("https://parcial2back-two.vercel.app/tasks");
+export const getTasksRequest = async () => axios.get("https://parcial2back-theta.vercel.app/tasks");
 
-export const createTaskRequest = async (task) => axios.post("https://parcial2back-two.vercel.app/auth/logintasks", task);
+export const createTaskRequest = async (task) => axios.post("https://parcial2back-theta.vercel.app/auth/logintasks", task);
 
 export const updateTaskRequest = async (task) =>
-  axios.put(`https://parcial2back-two.vercel.app/auth/login/tasks/${task._id}`, task);
+  axios.put(`https://parcial2back-theta.vercel.app/auth/login/tasks/${task._id}`, task);
 
-export const deleteTaskRequest = async (id) => axios.delete(`https://parcial2back-two.vercel.app/auth/login/tasks/${id}`);
+export const deleteTaskRequest = async (id) => axios.delete(`https://parcial2back-theta.vercel.app/auth/login/tasks/${id}`);
 
-export const getTaskRequest = async (id) => axios.get(`https://parcial2back-two.vercel.app/auth/login/tasks/${id}`);
+export const getTaskRequest = async (id) => axios.get(`https://parcial2back-theta.vercel.app/auth/login/tasks/${id}`);
--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL|| "https://parcial2back-two.vercel.app/";
+export const API_URL = import.meta.env.VITE_API_URL|| "https://parcial2back-theta.vercel.app/";
