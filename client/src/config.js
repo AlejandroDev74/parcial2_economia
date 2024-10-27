9e93857af8d7053aaf0b6481b8d683a04dfd33a0
AlejandroDev74 <calderonalejandrodev@gmail.com>
2024-10-27 00:32:14 -0500

    commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL|| "https://parcial2back-kappa.vercel.app/";
+export const API_URL = import.meta.env.VITE_API_URL|| "https://parcial2back-kappa.vercel.app/api/";
