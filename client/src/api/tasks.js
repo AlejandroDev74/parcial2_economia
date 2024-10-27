c3fe646f4f9a09a2c709a649d4890018040b9418
AlejandroDev74 <calderonalejandrodev@gmail.com>
2024-10-26 22:15:22 -0500

    Ajustes adicionales AlejandroDev74 26/10/2024

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -1,12 +1,12 @@
 import axios from "./axios";
 
-export const getTasksRequest = async () => axios.get("/tasks");
+export const getTasksRequest = async () => axios.get("https://parcial2back-rust.vercel.app/tasks");
 
-export const createTaskRequest = async (task) => axios.post("/tasks", task);
+export const createTaskRequest = async (task) => axios.post("https://parcial2back-rust.vercel.app/auth/logintasks", task);
 
 export const updateTaskRequest = async (task) =>
-  axios.put(`/tasks/${task._id}`, task);
+  axios.put(`https://parcial2back-rust.vercel.app/auth/login/tasks/${task._id}`, task);
 
-export const deleteTaskRequest = async (id) => axios.delete(`/tasks/${id}`);
+export const deleteTaskRequest = async (id) => axios.delete(`https://parcial2back-rust.vercel.app/auth/login/tasks/${id}`);
 
-export const getTaskRequest = async (id) => axios.get(`/tasks/${id}`);
+export const getTaskRequest = async (id) => axios.get(`https://parcial2back-rust.vercel.app/auth/login/tasks/${id}`);
